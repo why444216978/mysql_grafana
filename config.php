f152d9b2128ce2f7785ba870b7244367954097e9
weihaoyu <weihaoyu@ymt360.com>
2020-11-18 21:28:46 +0800

    config

--- config.php
+++ config.php
@@ -1,5 +1,8 @@
 <?php
+//注意：添加监控字段，需对应表中添加相应映射字段。
+
 return [
+    //对应 show global status like 'db_select' 等
     'status_fields'    => [
         'Com_select'           => 'db_select',
         'Com_insert'           => 'db_insert',
@@ -9,6 +12,8 @@ return [
         'Threads_connected'    => 'threads_connected',
         'Max_used_connections' => 'max_used_connections',
     ],
+
+    //对应 show global variables like 'db_select' 等
     'variables_fields' => [
         'max_connections' => 'max_connections',
         'version'         => 'db_version',
